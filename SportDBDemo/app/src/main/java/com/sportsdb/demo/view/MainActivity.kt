002fd429776ddc70e2f6387fe94b7297209d8386
Hardik Fumakiya <hbfumakiya@gmail.com>
2021-01-10 16:34:11 -0500

    creating common structure for the project, adding Constant classes for the app

--- SportDBDemo/app/src/main/java/com/sportsdb/demo/view/MainActivity.kt
+++ SportDBDemo/app/src/main/java/com/sportsdb/demo/view/MainActivity.kt
@@ -1,7 +1,13 @@
-package com.sportsdb.demo
+package com.sportsdb.demo.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sportsdb.demo.R
+
+/**
+ * Created by Hardik on 10-01-2021.
+ * this is launcher activity for app
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
